8ec705a750e9f80035ac06c892f62dec9b492a28
G_abstract <gabo20001@gmai.com>
2021-08-31 23:23:42 -0500

    Creación de los documentos html y js de el primer taller asi como escribiendo los primeros pasos

---
+++ figuras.js
@@ -0,0 +1,6 @@
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden" + " " + ladoCuadrado + "cm");
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es " + " " + perimetroCuadrado  + "cm")
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es " + " " + areaCuadrado  + "cm²")
