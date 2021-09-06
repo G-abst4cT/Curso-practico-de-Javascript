b405a40e77afa34b8c9e913a30336c3511312220
G_abstract <gabo20001@gmai.com>
2021-09-06 18:11:17 -0500

    Movimiento de archivos

--- Decuentos-y-porcentajes/decuentos.js
+++
@@ -1,9 +0,0 @@
-function calcularPorcentaje(){
-    const originalPrice = Number(document.getElementById("inputPrice").value);
-    const discount = Number(document.getElementById("inputDiscount").value);
-    const finalPrice = ((originalPrice* (100-discount))/100);
-    console.log(finalPrice);
-    document.getElementById("totalPrice").innerHTML = finalPrice + "$"
-}
-
-
